f6a0e2a155d605b00e5012b51d7a6be133771eb0
charles-meyer <charles.meyer@tufts.edu>
2014-02-02 05:26:26 -0500

    WOO COLORS REAL RANDOM GRAPH SWITCHING!

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
 	function rando(numelements){
 		return numelements*Math.random(0,1);
 	};
-	var LorR = rando(1);
-	var left ="not obama";
-	var right="not obama";
+	var LorR = rando(1);	
+	var left ="not " + thistopic.getName();
+	var right="not " + thistopic.getName();
 	if(LorR<.5)
-		left = obama.getName();
-	else right = obama.getName();
+		left = thistopic.getName();
+	else right = thistopic.getName();
 	document.getElementById('rightanswer').innerHTML= right;
 	document.getElementById('leftanswer').innerHTML= left;
 	alert("OH NOOOOO FUCKKKKKKK!!!!");
