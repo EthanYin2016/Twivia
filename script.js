734adc8534b7c2771c748dd42caa54f41139a75c
charles-meyer <charles.meyer@tufts.edu>
2014-02-02 00:07:53 -0500

    working graph?

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ $(document).ready(function () {
 	function rando(numelements){
 		return Math.floor(numelements*Math.random(0,1));
 	};
+	function fake_date_make{
+		return "02042005";
+	};
+	var fake_date = fake_date_make();
 	document.getElementById('question').innerHTML 
 	= "When was " + topics[rando(3)] + questionsend[rando(2)];
+	document.getElementById('leftanswer').innerHTML= fake_date;
 });
 
